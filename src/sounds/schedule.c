c34ca13cbedc6bdb9a3930ea022522dc408dfd93
Mariia Zhytnikova <allusio@gmail.com>
2025-08-29 11:39:31 +0300

    add synthesizer

---
+++ src/sounds/schedule.c
@@ -0,0 +1,93 @@
+#include "minisynth.h"
+
+int pitch_to_semitone(char pitch) {
+	switch(pitch) {
+		case 'c': return 0;
+		case 'd': return 2;
+		case 'e': return 4;
+		case 'f': return 5;
+		case 'g': return 7;
+		case 'a': return 9;
+		case 'b': return 11;
+		default: return -1; // r
+	}
+}
+
+/*pitch, alteration, octave to frequency*/
+
+double note_to_freq(t_note n)
+{
+	int	semitone;
+	int	octave;
+	int	midi;
+	int	freq;
+
+	if(n.pitch == 'r')
+		return 0.0; // пауза
+	semitone = pitch_to_semitone(n.pitch);
+	if(n.alteration == '#')
+		semitone += 1;
+	if(n.alteration == 'b')
+		semitone -= 1;
+	octave = n.octave - '0';
+	midi = (octave + 1)*12 + semitone;
+	freq = 440.0 * pow(2.0, (midi - 69) / 12.0);
+	return (freq);
+}
+
+double max_duration(t_schedule *schedule)
+{
+	double	max_time;
+	double	last;
+
+	max_time = 0.0;
+	for (int i = 0; i < schedule->track_count; i++) {
+		if (schedule->tracks[i].note_count > 0) {
+			last = schedule->tracks[i].notes[schedule->tracks[i].note_count - 1].end_s;
+			if (last > max_time)
+				max_time = last;
+		}
+	}
+	return (max_time);
+}
+
+int get_schedule(t_schedule *schedule, t_song *song)
+{
+	double seconds_per_beat;
+	double time;
+
+	seconds_per_beat = 60.0 / song->tempo;
+	schedule->track_count = song->track_count; // from parsed data
+	schedule->tracks = malloc(schedule->track_count * sizeof(t_scheduled_track));
+	if (!schedule->tracks)
+	{
+		// perror("malloc failed");  ERROR MSG
+		return (-1);
+	}
+	for (int i = 0; i < schedule->track_count; i++) {
+		schedule->tracks[i].note_count = song->tracks[i].note_count;
+		schedule->tracks[i].notes = malloc(
+			sizeof(t_scheduled_note) * schedule->tracks[i].note_count);
+		if (!schedule->tracks)
+		{
+			// perror("malloc failed");  ERROR MSG
+			//free schedule->tracks[i].notes for all done i
+			//free schedule->tracks 
+			return (-1);
+		}
+		schedule->tracks[i].instrument = song->tracks[i].instrument;
+		time = 0.0;
+		for (int j = 0; j < song->tracks[i].note_count; j++) {
+			schedule->tracks[i].notes[j].is_rest = 0;
+			schedule->tracks[i].notes[j].freq_hz = note_to_freq(song->tracks[i].notes[j]);
+			if (schedule->tracks[i].notes[j].freq_hz == 0.0)
+				schedule->tracks[i].notes[j].is_rest = 1;
+			schedule->tracks[i].notes[j].start_s = time;
+			schedule->tracks[i].notes[j].end_s = schedule->tracks[i].notes[j].end_s
+					+ song->tracks[i].notes[j].duration * seconds_per_beat;
+			time = schedule->tracks[i].notes[j].end_s;
+		}
+	}
+	schedule->song_duration = max_duration(schedule);
+	return (1);
+}
---
+++ src/sounds/synthesizer.c
@@ -0,0 +1,61 @@
+#include "minisynth.h"
+
+static int paCallback(const void *inputBuffer, void *outputBuffer,
+					unsigned long framesPerBuffer,
+					const PaStreamCallbackTimeInfo* timeInfo,
+					PaStreamCallbackFlags statusFlags,
+					void *userData)
+{
+	int16_t *out;
+	t_schedule *sched;
+	double callback_time; 
+
+	(void) inputBuffer;						//microphone
+	callback_time = 0.0;
+	sched = (t_schedule*)userData;
+	out = (int16_t*)outputBuffer;
+
+	for (unsigned long i = 0; i < framesPerBuffer; i++)		// Loop over buffer
+	{
+		double sample = 0.0;
+		for (int t_idx = 0; t_idx < sched->track_count; t_idx++)	// Loop over all tracks
+		{
+			t_scheduled_track *track = &sched->tracks[t_idx];
+			for (int n = 0; n < track->note_count; n++)
+			{
+				t_scheduled_note *note = &track->notes[n];
+				if (callback_time >= note->start_s && callback_time < note->end_s)
+				{
+					if (!note->is_rest) {
+						double phase = TWO_PI * note->freq_hz * (callback_time - note->start_s);
+						switch (track->instrument) {
+							case SINE:
+								sample += sin(phase);
+								break;
+							case SQUARE:
+								sample += (sin(phase) >= 0 ? 1.0 : -1.0);
+								break;
+							case SAW:
+								sample += 2.0*(phase/(TWO_PI) - floor(phase/(TWO_PI) + 0.5));
+								break;
+							case TRIANGLE:
+								sample += asin(sin(phase))*PI_TWO;
+								break;
+						}
+					}
+					break;		// only one note active at a time per track
+				}
+			}
+		}
+		// Scale down volume to prevent clipping
+		sample *= 0.2;
+		if (sample > 1.0) sample = 1.0;
+		if (sample < -1.0) sample = -1.0;
+
+		int16_t out_sample = (int16_t)(sample * 32767);
+		*out++ = out_sample; // left
+		*out++ = out_sample; // right
+		callback_time += 1.0 / SAMPLE_RATE;
+	}
+	return paContinue; // keep playing
+}
